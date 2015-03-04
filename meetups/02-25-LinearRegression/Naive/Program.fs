7ca7a13b042a7473a1e5e036e3c5e607fff1756c
John Donnellan <john@blueTribu.com>
2015-03-04 23:08:50 +0000

    solve for both a and c

--- meetups/02-25-LinearRegression/Naive/Program.fs
+++ meetups/02-25-LinearRegression/Naive/Program.fs
@@ -1,22 +1,11 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-// y=0.06391x + 0.750029999999
-
-open System
+﻿open System
 open System.IO
-
-let readLines (filePath:string) =  [
-    use sr = new StreamReader (filePath)
-    while not sr.EndOfStream do
-        let line = sr.ReadLine ()
-        yield Double.Parse(line)
-]
 
 type datum = {age:float; height:float}
 
-let linearGuess (data: datum list) =
-    let first = data.Head
-    let last = List.reduce (fun _ i -> i) data
+let linearGuess (data: datum array) =
+    let first = data.[0]
+    let last = data.[data.Length - 1]
     if first.height = last.height then
         (0.0, first.height)
     else
@@ -24,56 +13,50 @@ let linearGuess (data: datum list) =
         let c = first.height - (first.age * a)
         (a, c)            
 
-let costDatum a c (dtm:datum)  =
-    let est = a * dtm.age + c
-    let error = est - dtm.height
+let costOfDatum a c datum  =
+    let error = datum.height - (a * datum.age + c)
     error * error
 
-let cost (data:datum list) a c =
-    data 
-    |> List.map (costDatum a c)
-    |> List.sum
+let costOfData data a c =
+    data |> Array.sumBy (costOfDatum a c)
 
-let rec climbHill (f: float -> float) (x:float) (step:float) = seq{   
-    let y = f x
+let rec climbHill func x y step = seq{
     yield (x, y, step)
-    let x' = x + step
-    let y' = f x'
-    let step' = if y' > y then step else -step/10.0
-    yield! climbHill f x' step'       
+
+    let best = 
+        [(x+step, y); (x, y+step); (x-step, y); (x, y-step)]
+        |> List.map (fun (x, y) -> (x, y, func x y))
+        |> List.sortBy (fun (_, _, value) -> - value)
+        |> List.head
+
+    yield! match best with
+            | (x', y', value') when value' > func x y -> climbHill func x' y' step
+            | (_, _, value')  -> climbHill func x y (step/10.0)
 }    
 
 [<EntryPoint>]
 let main argv = 
-    let appDir =  DirectoryInfo( __SOURCE_DIRECTORY__).Parent
-    let dataDir = Path.Combine(appDir.FullName, "Data")
-    
-    let ageFile = Path.Combine(dataDir, "ex2x.dat")
-    let heightFile = Path.Combine(dataDir, "ex2y.dat")
+    Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-    printfn "%s" ageFile
+    let readFloats file =  
+        File.ReadAllLines file
+        |> Array.map float
 
-    let ages = readLines ageFile
-    let heights = readLines heightFile
-
-    let sampleData = List.zip ages heights |> List.map(fun (a, h) -> {age=a; height = h})
-    
-    //let (aGuess, cGuess) = linearGuess(sampleData) 
-    let (aGuess, cGuess) = 1.0, 0.750029999999
-    
+    let sampleData = 
+        Array.zip (readFloats @"..\data\ex2x.dat") (readFloats @"..\data\ex2y.dat") 
+        |> Array.map(fun (a, h) -> {age=a; height = h})
+        
     let step = 1.0;
-    
-    let ourF x = -(cost sampleData x cGuess)
-    
-    let trail = climbHill ourF aGuess step
-    
-    let (a, cost, step) = 
+    let aGuess, cGuess = linearGuess(sampleData) 
+    let quality a c = -(costOfData sampleData a c)    
+    let trail = climbHill quality aGuess cGuess step
+   
+    let a, c, _ = 
         trail 
-        |> Seq.where (fun (_, _, step) -> abs step < 0.0000001)
+        |> Seq.where (fun (_, _, step) -> abs step < 0.00000000001)
         |> Seq.head
 
-    printfn "answer:%0.5f cost:%0.5f step:%0.10f" a cost step
+    printfn "a:%0.10f c:%0.10f" a c 
     
-    printfn "Press any key to close."
     Console.ReadKey() |> ignore
     0
