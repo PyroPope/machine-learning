0daf6868eeeb9d9b83bfc57b7d06a6dc4714b02f
John Donnellan <john@blueTribu.com>
2015-03-05 07:04:50 +0000

    tweaking around

--- meetups/02-25-LinearRegression/Naive/Program.fs
+++ meetups/02-25-LinearRegression/Naive/Program.fs
@@ -1,38 +1,37 @@
 ﻿open System
 open System.IO
 
-type datum = {age:float; height:float}
+type point = {x:float; y:float}          
 
-let linearGuess (data: datum array) =
-    let first = data.[0]
-    let last = data.[data.Length - 1]
-    if first.height = last.height then
-        (0.0, first.height)
-    else
-        let a = (last.height - first.height)/(last.age - first.age)
-        let c = first.height - (first.age * a)
-        (a, c)            
-
-let costOfDatum a c datum  =
-    let error = datum.height - (a * datum.age + c)
-    error * error
-
-let costOfData data a c =
-    data |> Array.sumBy (costOfDatum a c)
+let costOfSequence data a c =
+    let costOfPoint point  =
+        (point.y - ( a * point.x + c)) ** 2.0
+    data |> Array.sumBy costOfPoint 
 
 let rec climbHill func x y step = seq{
     yield (x, y, step)
 
     let best = 
         [(x+step, y); (x, y+step); (x-step, y); (x, y-step)]
-        |> List.map (fun (x, y) -> (x, y, func x y))
-        |> List.sortBy (fun (_, _, value) -> - value)
-        |> List.head
+        |> List.map (fun (x, y) -> (func x y, x, y))
+        |> List.max
 
     yield! match best with
-            | (x', y', value') when value' > func x y -> climbHill func x' y' step
-            | (_, _, value')  -> climbHill func x y (step/10.0)
-}    
+            | (value', x', y') when value' > func x y -> climbHill func x' y' step
+            | (value', _, _)  -> climbHill func x y (step/10.0)
+}  
+
+let guess (data: point array) =
+    let step = List.max <| [for d in data do yield abs d.y] 
+    let first = data.[0]
+    let last = data.[data.Length - 1]
+    if first.y = last.y then
+        (0.0, first.y, step)
+    else
+        let a = (last.y - first.y)/(last.x - first.x)
+        let c = first.y - (first.x * a)
+        (a, c, step)    
+
 
 [<EntryPoint>]
 let main argv = 
@@ -44,19 +43,20 @@ let main argv =
 
     let sampleData = 
         Array.zip (readFloats @"..\data\ex2x.dat") (readFloats @"..\data\ex2y.dat") 
-        |> Array.map(fun (a, h) -> {age=a; height = h})
+        |> Array.map(fun (a, h) -> {x=a; y = h})
         
-    let step = 1.0;
-    let aGuess, cGuess = linearGuess(sampleData) 
-    let quality a c = -(costOfData sampleData a c)    
+    let minStep = 0.000000001
+    let aGuess, cGuess, step = guess sampleData
+    printfn "Starting with: a=%f c=%f step=%f" aGuess cGuess step 
+    let quality a c = -(costOfSequence sampleData a c)    
     let trail = climbHill quality aGuess cGuess step
    
-    let a, c, _ = 
-        trail 
-        |> Seq.where (fun (_, _, step) -> abs step < 0.00000000001)
-        |> Seq.head
+    let a, c, _ = trail 
+                    |> Seq.where (fun (_, _, step) -> abs step < minStep)
+                    |> Seq.head
 
-    printfn "a:%0.10f c:%0.10f" a c 
-    
+    printfn ""
+    printfn "Answer: a=%0.8f c=%0.8f" a c 
+
     Console.ReadKey() |> ignore
     0
