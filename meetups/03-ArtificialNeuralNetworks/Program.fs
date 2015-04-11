668ed8115cf6419048153251aa2032e31f83d976
John Donnellan <john@blueTribu.com>
2015-04-11 12:28:45 +0100

    03:  empty Neural Net

---
+++ meetups/03-ArtificialNeuralNetworks/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let getNetwork inputCount hiddenCount =
+    fun (inputs : float array) -> Array.sum inputs
+
+
+[<EntryPoint>]
+let main argv = 
+    let net = getNetwork 0 0;
+    let out = net [|1.0; 2.0; 3.0; 4.0|]
+
+    Console.WriteLine out
+    Console.ReadKey() |> ignore
+    0 
