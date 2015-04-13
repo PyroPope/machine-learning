8c04e895231d304e811031ce1c716be6c3800b76
John Donnellan <john@blueTribu.com>
2015-04-13 11:58:29 +0100

    add book code examples

---
+++ books/PCI/chapter2/recommendations-my.py
@@ -0,0 +1,36 @@
+# A dictionary of movie critics and their ratings of a small
+# set of movies
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 
+ 'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 
+ 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 
+ 'You, Me and Dupree': 3.5}, 
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+ 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+ 'The Night Listener': 4.5, 'Superman Returns': 4.0, 
+ 'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 
+ 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 2.0}, 
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+from math import sqrt
+
+def sq (x):
+  return pow(x, 2)
+
+def simDistance(films, critName1, critName2):
+  critic1 = critics[critName1]
+  critic2 = critics[critName2]
+  total = 0
+  for film in films:
+    if film in critic1 and film in critic2:
+      total +=  sq(critic1[film] - critic2[film])
+  return 1/(1 + sqrt(total / len(films)))
+
+
+print simDistance ( ['Snakes on a Plane'], 'Lisa Rose', 'Toby' )
