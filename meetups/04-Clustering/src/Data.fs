8f4aba60ceac854b53ddb0a457200f29f0fa3b60
John Donnellan <john@blueTribu.com>
2015-05-27 15:39:21 +0100

    04:  new project -> getCentroids

---
+++ meetups/04-Clustering/src/Data.fs
@@ -0,0 +1,29 @@
+﻿module Data
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+let private getChildDirectory childName = 
+    let appRoot = DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent
+    Path.Combine(appRoot.FullName, childName)
+    |> Directory.CreateDirectory
+    |> (fun di -> di.FullName)
+
+let dataDir = getChildDirectory "data"
+
+
+let readLines filename = 
+    let skipLineRgx = new Regex(@"(^\s*$|^\s*#)", RegexOptions.Compiled)
+    Path.Combine(dataDir, filename)
+        |> File.ReadAllLines
+        //|> Array.filter (fun line -> not (skipLineRgx.IsMatch(line)))
+        |> Array.filter ((skipLineRgx.IsMatch) >> not)
+    
+    
+let readRows filename =
+    let splitRgx = new Regex(@"\s*[,]\s*", RegexOptions.Compiled)
+    readLines filename
+        |> Array.map (splitRgx.Split)
+
+        
---
+++ meetups/04-Clustering/src/Program.fs
@@ -0,0 +1,54 @@
+﻿open System
+
+type Iris = {
+    Vector : float[]
+    SepalLength : float
+    SepalWidth : float
+    PetalLength : float
+    PetalWidth : float
+    Class : string
+}
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Hello, got args: %A" argv
+
+    let samples =
+        Data.readRows "iris.data" 
+        |> Array.map (fun row -> 
+        let vector = [|0..3|] |> Array.map (fun i -> float row.[i])
+        {   Vector = vector;
+            SepalLength = vector.[0]
+            SepalWidth = vector.[1]
+            PetalLength = vector.[2]
+            PetalWidth = vector.[3]
+            Class = row.[4] })
+
+    let getMinsAndMaxs vectors = 
+        let first = Seq.head vectors
+        let mins = Array.copy first
+        let maxs = Array.copy first
+        vectors
+        |> Seq.iter (Array.iteri (fun i v ->
+            mins.[i] <- min mins.[i] v
+            maxs.[i] <- max maxs.[i] v
+        ))            
+        mins, maxs
+        
+    let getCentroids count vectors =
+        let mins, maxs = getMinsAndMaxs vectors  
+        let difs = (maxs, mins) ||> Array.map2 (-)
+        let rnd = new Random()
+        let getCentroid() = 
+            (difs, mins)
+            ||> Array.map2 (fun dif min -> rnd.NextDouble() * dif + min)
+        [|1..count|] |> Array.map (fun _ -> getCentroid())
+            
+
+    let vectors = samples |> Array.map (fun s -> s.Vector)
+    let centroids = getCentroids 3 vectors
+    printfn "%A" centroids
+
+    printfn "All done."; System.Console.ReadKey() |> ignore
+    0
+
